7cea424ad6395c71facbcf89b900fa76a825126d
0xbea <bea_z@live.com>
2021-06-01 17:17:13 +0200

    Update Buy_DCA.js
    changed to limit order

--- Buy_DCA.js
+++ Buy_DCA.js
@@ -3,6 +3,7 @@ function buyDCA(pair,amount) {
   for (name in api_data['result']) {
     bid = api_data['result'][name]['b'][0]
   }
+  price = bid
   volume = amount / bid
-  KAPI_AddOrder('Account', pair, 'buy', 'market', volume)
+  KAPI_AddOrder('Account', pair, 'buy', 'limit', volume, price)
 }
